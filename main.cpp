729d8d0b3a6a28b540d48f8da22b9aa3aef04787
PiotrWolinski <55030310+PiotrWolinski@users.noreply.github.com>
2020-04-17 17:49:14 +0200

    Reading input is now working

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Tree {
 	int value;
@@ -17,47 +19,92 @@ void init_Tree(Tree* root) {
 	root->son = NULL;
 }
 
-void read_NEWICK(Tree* root, char* input) {
-	init_Tree(root);
-	int size = 10;
-	input = (char*)malloc(size * sizeof(char));
-	Tree* tmp_tree; 
+Tree* alloc() {
+	return (Tree*)malloc(sizeof(Tree));
+}
 
-	if (input != NULL) {
-		char tmp;
-		char last;
-		int i = 0;
-		while ((tmp = getchar()) != ';') {
-			if (tmp != '(') {
-				if (tmp_tree->son == NULL) {
-					tmp_tree->son = (Tree*)malloc(sizeof(Tree));
-				}
-				else if (tmp_tree->son != NULL) {
-					tmp_tree = tmp_tree->son;
-					if (tmp_tree->brother == NULL) {
-						tmp_tree->brother = (Tree*)malloc(sizeof(Tree));
-					}
-					else {
-						do {
-							tmp_tree = tmp_tree->brother;
-						} while (tmp_tree->brother == NULL);
-					}
-				}
-				root->son = (Tree*)malloc(sizeof(Tree));
-				tmp_tree = root->son;
-				init_Tree(tmp_tree);
-			}
-			last = tmp;
+Tree read_NEWICK(char* input) {
+	Tree* tmp_tree = alloc();
+	init_Tree(tmp_tree);
+
+	int i = 0;
+	do {
+		if (input[i] == '(') {
+			tmp_tree->son = alloc();
+			init_Tree(tmp_tree->son);
+			tmp_tree->son->parent = tmp_tree;
+			tmp_tree = tmp_tree->son;
 		}
-	}
-
-
-
+		else if (input[i] == ',') {
+			tmp_tree->brother = alloc();
+			init_Tree(tmp_tree->brother);
+			tmp_tree->brother->parent = tmp_tree->parent;
+			tmp_tree = tmp_tree->brother;
+		}
+		else if (input[i] == ')') {
+			tmp_tree = tmp_tree->parent;
+		}
+		else {
+			//liczba dwucyfrowa
+			if (input[i] == '1' && input[i + 1] > 48 && input[i + 1] < 58) {
+				tmp_tree->value = 10 + input[i + 1] - 48;
+				i++;
+			} 
+			else {
+				tmp_tree->value = input[i] - 48;
+			}
+		}
+		i++;
+	} while (input[i] != ';');
+	
+	return *tmp_tree;
 }
 
 int main() {
+	int size;
+	std::cin >> size;
+
+	Tree* array = (Tree*)malloc(size * sizeof(Tree));
+	char** input = (char**)malloc(size * sizeof(char*));
+	int i = 0;
+	int blank = getchar();
+	do
+	{
+		unsigned int arr_size = 1;
+		input[i] = (char*)malloc(sizeof(char));
+
+		if (input[i] != NULL)
+		{
+			char q;
+			unsigned int j = 0;
+			while ((q = getchar()) != '\n')
+			{
+				input[i][j++] = q;
+				if (j == arr_size)
+				{
+					arr_size = j + 1;
+					if (char* tmp = (char*)realloc(input[i], arr_size))
+					{
+						input[i] = tmp;
+					}
+				}
+			}
+			input[i][j] = '\0';
+			i++;
+		}
+	} while (i < size);
+	for (int i = 0; i < size; i++) {
+		array[i] = read_NEWICK(input[i]);
+	}
 
 
+	for (int i = 0; i < size; i++)
+	{
+		printf("%s\n", input[i]);
+	}
 
+	for (int i = 0; i < size; i++) {
+		free(input[i]);
+	}
 	return 0;
 }
